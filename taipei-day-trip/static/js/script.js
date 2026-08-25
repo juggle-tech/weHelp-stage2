5c7471dacdb3c4897c59b26f67722688532a820f
juggle-tech <shareitaga@gmail.com>
2026-08-26 00:39:21 +0800

    Completed pop-up dialogs for sign-in and sign-up

--- taipei-day-trip/static/js/script.js
+++ taipei-day-trip/static/js/script.js
@@ -1,15 +1,48 @@
+/**
+ * 
+ */
 
 // Toggle signin and signup pop-up dialogues
+const signinPopup = document.querySelector(".signinPopup");
+const signupPopup = document.querySelector(".signupPopup");
 
+document.getElementById("signInStatus").addEventListener("click", function(event) {
+    event.preventDefault();
+    signinPopup.classList.add("open");
+});
+
+// Change to Signup pop-up dialogue
+document.getElementById("signinText").addEventListener("click", function() {
+    signinPopup.classList.remove("open");
+    signupPopup.classList.add("open");
+    document.getElementById("signinError").textContent = "";
+});
+
+// Change to Signin pop-up dialogue
+document.getElementById("signupText").addEventListener("click", function() {
+    signupPopup.classList.remove("open");
+    signinPopup.classList.add("open");
+    document.getElementById("signupError").textContent = "";
+});
+
+// Close Signin / Signup pop-up dialogues
+document.getElementById("signinX").addEventListener("click", function() {
+    signinPopup.classList.remove("open");
+    document.getElementById("signinError").textContent = "";
+});
+
+document.getElementById("signupX").addEventListener("click", function() {
+    signupPopup.classList.remove("open");
+    document.getElementById("signupError").textContent = "";
+});
 
 
 // User sign in 
-document.getElementById("signinBtn").addEventListener("click", async function(event) {
+document.getElementById("signinBtn").addEventListener("click", async function() {
     const email = document.getElementById("emailSignin").value.trim();
     const password = document.getElementById("passwordSignin").value.trim();
 
     if (!email || !password) {
-        event.preventDefault();
         alert("Email and password can not be empty!");
         return;
     }
@@ -23,27 +56,28 @@ document.getElementById("signinBtn").addEventListener("click", async function(ev
 
         // Sign in successfully
         if (result.data) {
+            // Store token and change status
             localStorage.setItem("token", result.token);
             location.reload();
-
             document.getElementById("signInStatus").textContent = "登出系統";
+            // document.getElementById("signinError").textContent = "";
         } else {
+            // Show error message
             document.getElementById("signinError").textContent = "帳號或密碼輸入錯誤";
         }
     } catch (err) {
-        console.error("Signin fails:", err);
+        console.error("Sign in fails:", err);
     }
 });
 
 
 // User sign up
-document.getElementById("signupBtn").addEventListener("click", async function(event) {
+document.getElementById("signupBtn").addEventListener("click", async function() {
     const name = document.getElementById("name").value.trim();
     const email = document.getElementById("emailSignup").value.trim();
     const password = document.getElementById("passwordSignup").value.trim();
 
     if (!name || !email || !password) {
-        event.preventDefault();
         alert("No input can be empty!");
         return;
     }
@@ -60,10 +94,14 @@ document.getElementById("signupBtn").addEventListener("click", async function(ev
         // Sign in successfully
         if (result.ok) {
             // Close pop-up and reset all values
+            signupPopup.classList.remove("open");
+            document.getElementById("signupForm").reset();
+            document.getElementById("signupError").textContent = "";
         } else {
+            // Show error message
             document.getElementById("signupError").textContent = "帳號或密碼輸入錯誤";
         }
     } catch (err) {
-        console.error("Signin fails:", err);
+        console.error("Sign up fails:", err);
     }
 });
